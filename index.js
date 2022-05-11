3e9b6533c916c03f7f2e3919bd3a3cce83f36de8
rushi1222 <k.rushi1222@gmail.com>
2022-05-11 18:26:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const redux  = require('redux')
+
+const createStore = redux.createStore
+
+const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+
+
+function orderCake() {
+    return {
+        type: CAKE_ORDERED,
+        payload: 1,
+    }
+}
+
+function restockCake(qnt = 1) {
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qnt,
+    }
+}
+
+
+const initialState = {
+    numOfCakes: 10,
+}
+
+const reducer = (state = initialState, action)=>{
+    switch(action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes-1,
+            }
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes+ action.payload,
+            }        
+            default:
+            return state
+    }
+}
+
+const store = createStore(reducer)
+console.log('Initial state', store.getState())
+
+const unsubscribe = store.subscribe(()=>
+console.log('Upadate state', store.getState())
+)
+
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(restockCake(3))
+
+
+unsubscribe()
